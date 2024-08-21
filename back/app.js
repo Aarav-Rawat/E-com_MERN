bfd663502f5ac8daffcf50a61b45837f7d1cc341
AaraV <aaravrawat00000@gmail.com>
2024-08-21 15:31:25 +0530

    front url changed

--- back/app.js
+++ back/app.js
@@ -18,7 +18,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://verzatilexshopz.onrender.com',
   credentials: true,
 }));
 
