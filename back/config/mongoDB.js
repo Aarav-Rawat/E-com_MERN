ae4fe3e0f4aa747f4c1fc18b6315be46c87e06f0
AaraV <aaravrawat00000@gmail.com>
2024-09-11 10:50:36 +0530

    compass to atlas

--- back/config/mongoDB.js
+++ back/config/mongoDB.js
@@ -1,27 +1,27 @@
 import mongoose from "mongoose";
 
-const connectDb = async () => {
-  try {
-      const conn = await mongoose.connect(process.env.MongoDb_Compass_URI, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-      });
-      console.log(`Connected to compass`);
-  } catch (err) {
-      console.error(`Not connected to compass`);
-      process.exit(1); 
-  }
-}
-
 // const connectDb = async () => {
-//     try {
-//       await mongoose.connect(process.env.MongoDb_Atlas_URI);
-//       console.log("connection successful to DB");
-//     } catch (error) {
-//       console.error("database connection failed",error);
-//       process.exit(1);
-//     }
-//   };
+//   try {
+//       const conn = await mongoose.connect(process.env.MongoDb_Compass_URI, {
+//           useNewUrlParser: true,
+//           useUnifiedTopology: true,
+//       });
+//       console.log(`Connected to compass`);
+//   } catch (err) {
+//       console.error(`Not connected to compass`);
+//       process.exit(1); 
+//   }
+// }
+
+const connectDb = async () => {
+    try {
+      await mongoose.connect(process.env.MongoDb_Atlas_URI);
+      console.log("connection successful to DB");
+    } catch (error) {
+      console.error("database connection failed",error);
+      process.exit(1);
+    }
+  };
 
 
 export default connectDb;
