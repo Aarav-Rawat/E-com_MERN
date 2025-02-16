8e5caa167a6c185c100567da2cfdab22cbb41d75
Aarav-Rawat <aaravrawat00000@gmail.com>
2025-02-16 21:09:51 +0530

    db changed

--- Backend/config/mongoDB.js
+++ Backend/config/mongoDB.js
@@ -2,28 +2,28 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const connectDb = async () => {
-  try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/E-Com", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (err) {
-    console.error(`Error: ${err.message}`);
-    process.exit(1);
-  }
-};
-
 // const connectDb = async () => {
-//     try {
-//       await mongoose.connect(process.env.MongoDb_Atlas_URI);
-//       console.log("connection successful to DB");
-//     } catch (error) {
-//       console.error("database connection failed",error);
-//       process.exit(1);
-//     }
-//   };
+//   try {
+//     const conn = await mongoose.connect("mongodb://localhost:27017/E-Com", {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (err) {
+//     console.error(`Error: ${err.message}`);
+//     process.exit(1);
+//   }
+// };
+
+const connectDb = async () => {
+    try {
+      await mongoose.connect(process.env.MongoDb_Atlas_URI);
+      console.log("connection successful to DB");
+    } catch (error) {
+      console.error("database connection failed",error);
+      process.exit(1);
+    }
+  };
 
 export default connectDb;
 
